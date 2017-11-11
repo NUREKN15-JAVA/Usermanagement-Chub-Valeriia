710ca60f35ef2432e6807e32c97a580823d3d461
Valeriia Chub <valeriia.chub1@nure.ua>
2017-11-11 11:18:35 +0200

    minor fixes

--- src/main/java/ua/nure/chub/MyBundle.java
+++ src/main/java/ua/nure/chub/MyBundle.java
@@ -1,6 +1,7 @@
 package main.java.ua.nure.chub;
 
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 import java.text.MessageFormat;
 
@@ -9,8 +10,8 @@ import java.text.MessageFormat;
  * created 04.11.2017.
  */
 public class MyBundle {
-    private static final ResourceBundle bundle = ResourceBundle.getBundle(
-            "words");
+    private static final String FILENAME = "main//resources//words";
+    private static ResourceBundle bundle = ResourceBundle.getBundle(FILENAME, Locale.getDefault());
 
     public static String getString(String key, Object... params) {
         String value = bundle.getString(key);
